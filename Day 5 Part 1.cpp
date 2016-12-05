f5ea912b2150330153886584cf6f71b178ce82fa
Nathan Oliver <noliver@plaspros.com>
2016-12-05 12:58:25 -0600

    Add files via upload

---
+++ Day 5 Part 1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <sstream>
+#include <utility>
+#include "md5.h"
+
+int main()
+{
+	std::string input = "ugkcyxxp";
+	std::string builder;
+
+	for (std::size_t i = 0; builder.size() != 8; ++i)
+	{
+		std::string hex = md5(input + std::to_string(i));
+		if (hex[0] == '0' && hex[1] == '0' && hex[2] == '0' && hex[3] == '0' && hex[4] == '0')
+		{
+			builder += hex[5];
+		}
+	}
+	std::cout << builder;
+}
---
+++ Day 5 Part 2.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <sstream>
+#include <utility>
+#include <cctype>
+#include <set>
+#include "md5.h"
+
+int main()
+{
+	std::string input = "ugkcyxxp";
+	std::string builder(8, 0);
+	std::set<int> found;
+
+	for (std::size_t i = 0; found.size() != 8; ++i)
+	{
+		std::string hex = md5(input + std::to_string(i));
+		if (hex[0] == '0' && hex[1] == '0' && hex[2] == '0' && hex[3] == '0' && hex[4] == '0')
+		{
+			if (isdigit(hex[5]) && hex[5] < '8')
+			{
+				if (found.insert(hex[5]).second)
+					builder[hex[5] - '0'] = hex[6];
+			}
+		}
+	}
+	std::cout << builder;
+}
