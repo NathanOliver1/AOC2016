66b384212eb2796af4f67120daf45d299e49974c
Nathan Oliver <noliver@plaspros.com>
2016-12-06 07:04:52 -0600

    Add files via upload

---
+++ Day 6 Part 1.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <sstream>
+#include <utility>
+#include <cctype>
+#include <set>
+
+int main()
+{
+	std::vector<std::map<char, int>> messages(8);
+	std::ifstream fin("file.txt");
+	std::string line;
+	while (std::getline(fin, line))
+	{
+		for (std::size_t i = 0; i < line.size(); ++i)
+		{
+			messages[i][line[i]]++;
+		}
+	}
+	std::string builder;
+	for (std::size_t i = 0; i < messages.size(); ++i)
+	{
+		auto it = std::max_element(messages[i].begin(), messages[i].end(), [](const auto& lhs, const auto& rhs) {
+			return lhs.second < rhs.second;
+		});
+		builder += it->first;
+	}
+	std::cout << builder;
+}
